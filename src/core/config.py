878cb14211fa2bbd2a37482ca3641d7fba446761
Gealor <gzazulya@gmail.com>
2026-03-25 20:54:46 +0400

    update config

---
+++ src/core/config.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE = BASE_DIR / ".env"
+ENV_TEMPLATE = BASE_DIR / ".env.template"
+
+class RuntimeSettings(BaseModel):
+    host: str = '0.0.0.0'
+    port: int = 8000
+
+class DatabaseSettings(BaseModel):
+    db_name: Annotated[str, Field(alias="POSTGRES_DB")]
+    db_user: Annotated[str, Field(alias="POSTGRES_USER")]
+    db_password: Annotated[str, Field(alias="POSTGRES_PASSWORD")]
+    db_host: str = Field(default="localhost", alias="POSTGRES_HOST")
+    db_port: int = Field(default=6000, alias="POSTGRES_PORT")
+    db_echo: bool = False
+
+    @property
+    def db_url(self):
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(ENV_TEMPLATE, ENV_FILE),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+    )
+
+    runtime: RuntimeSettings = RuntimeSettings()
+    database: DatabaseSettings
+
+settings = Settings()
