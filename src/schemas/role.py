4a703390831b3b1d565b5340807829153084d9ae
Gealor <gzazulya@gmail.com>
2026-03-27 15:57:12 +0400

    fix imports in role pydantic schemas

--- src/schemas/role.py
+++ src/schemas/role.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 # BusinessElement schemas
 class BusinessElementSchemaBase(BaseModel):
