9c2c303a5ef0b5b489ee85eb4bc614195a2d34d8
Gealor <gzazulya@gmail.com>
2026-03-28 20:11:13 +0400

    add PermissionChecker

--- src/core/auth/security.py
+++ src/core/auth/security.py
@@ -7,7 +7,8 @@ from core.auth.creation_tokens import ACCESS_TOKEN_TYPE, decode_jwt
 from core.logger import log
 from core.database import db_session_getter
 from repositories.user_repository import UserRepository
-from schemas.user_schemas import UserRead
+from schemas.role_schemas import AccessRoleRuleRead
+from schemas.user_schemas import UserInfoForAdmin
 
 
 security = HTTPBearer()
@@ -16,12 +17,12 @@ security = HTTPBearer()
 async def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(security),
     db: AsyncSession = Depends(db_session_getter)
-) -> UserRead:
+) -> UserInfoForAdmin:
     token = credentials.credentials
     try:
         payload = decode_jwt(token)
-        
-        if type_token := payload.get("type") != ACCESS_TOKEN_TYPE:
+        type_token = payload.get("type")
+        if type_token != ACCESS_TOKEN_TYPE:
             log.error("An access token was expected, got %s", type_token)
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, 
@@ -41,7 +42,7 @@ async def get_current_user(
             detail="Invalid or expired token"
         )
 
-    user = await UserRepository(db_session=db).get_user_by_id(user_id=user_id)
+    user = await UserRepository(db_session=db).get_user_with_role(user_id=user_id)
 
     if not user or not user.is_active:
         raise HTTPException(
@@ -49,3 +50,38 @@ async def get_current_user(
             detail="The user was not found or deleted"
         )
     return user
+
+
+class PermissionChecker:
+    def __init__(self, element_name: str, action: str):
+        self.element_name = element_name
+        self.action = action 
+
+
+    def _found_target_rule(self, current_user: UserInfoForAdmin) -> AccessRoleRuleRead | None:
+        for rule in current_user.role.rules:
+            if rule.element.name == self.element_name:
+                return rule
+            
+        return None
+
+    async def __call__(
+        self, 
+        current_user: UserInfoForAdmin = Depends(get_current_user)
+    ):
+        # Правило для конкретного бизнес-модуля
+        target_rule = self._found_target_rule(current_user)
+
+        if not target_rule:
+            log.error("Not found '%s' element for role %s", self.element_name, current_user.role)
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access denied to this module")
+
+        # Проверка флагов (например, update_permission или update_all_permission)
+        base_perm = getattr(target_rule, f"{self.action}_permission", False)
+        all_perm = getattr(target_rule, f"{self.action}_all_permission", False)
+
+        if not (base_perm or all_perm):
+            log.error("Not found '%s' permission for role %s", self.action, current_user.role)
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Action forbidden")
+
+        return target_rule
